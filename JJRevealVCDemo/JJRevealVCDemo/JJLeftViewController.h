2ec71247bc0ff41627333bd53e8970525d944a5c
liujiajie <liujiajie@dianping.com>
2016-11-01 18:48:15 +0800

    【添加viewControllers】

--- JJRevealVCDemo/JJRevealVCDemo/JJLeftViewController.h
+++ JJRevealVCDemo/JJRevealVCDemo/JJLeftViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  JJLeftViewController.h
 //  JJRevealVCDemo
 //
 //  Created by 刘佳杰 on 16/11/1.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface JJLeftViewController : UIViewController
 
 @end
-
